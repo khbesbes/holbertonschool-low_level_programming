d5614a50739c13e8220e916e221256b7b9c24de7
khbesbes <5786@holbertonstudents.com>
2022-10-03 11:40:52 -0700

    first commit

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,7 +3,8 @@
 /**
  * printf is formatted output conversion
 */
-int main (void) {
-   printf("with proper grammar, but the outcome is a piece of art,");
+int main (void) 
+{
+   printf("with proper grammar, but the outcome is a piece of art,\n");
    return (0); 
-  }
+}
