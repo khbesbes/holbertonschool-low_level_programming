b5d226513c1677fd417a9575a445c912c52744e3
khbesbes <5786@holbertonstudents.com>
2022-10-03 05:14:07 -0700

    gcc -v

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-int putchar "Programming is like building a multilingual puzzle
+#include<stdio.h>
+/**
+ * main =write a C program prints
+ *
+ * Return:0
+ */  
+int main(void) 
+}
+puts("\"Programming is like building a multilingual puzzle");
+    return 0;
+}
+
