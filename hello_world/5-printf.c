513cbca06a06a53f38bf2b93e801d2dc7e582fd5
khbesbes <5786@holbertonstudents.com>
2022-10-03 11:23:00 -0700

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+/**
+ * printf is formatted output conversion
+ */
+int main (void);
+  {
+  printf("with proper grammar, but the outcome is a piece of art,");
+  Return (0);
+  } 
