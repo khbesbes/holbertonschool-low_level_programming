b07cc073ebfa03e49aad08e747ee549feef165b7
khbesbes <5786@holbertonstudents.com>
2022-10-04 03:06:07 -0700

    prints the size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,21 +2,21 @@
 /**
  * main - the progarm that prints the size of various types
  * return : 0
- */  
+ */
 int main(void)
 {
 	int intType;
 	float floatType;
 	long int intType;
-	long long int longType; 
+	long long int longType;
 	char charType;
 
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of char: %zu byte\n", sizeof(charType);
-	printf("size of long int: %zubyte\n",sizeof(intType));	
-	printf("size of long long int : %zubyte\n"sizeof(longinitType));
-	
-    
+	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("size of long int: %zubyte\n", sizeof(intType));
+	printf("size of long long int : %zubyte\n", sizeof(longinitType));
+
+
 	return (0);
 }
