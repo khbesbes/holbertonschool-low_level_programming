d0eba39f79f927f45b61ac260b82a8a9fa6270b5
khbesbes <5786@holbertonstudents.com>
2022-10-06 04:07:18 -0700

    numberz ascii

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- print count with putchar
+ * Return: 0
+ */
+int main(void)
+{
+
+	int N;
+
+	for (N = 48 ; N <= 57 ; N++)
+	{
+		putchar(N);
+	}
+	putchar('\n');
+
+	return (0);
+}
