57d80657dd86f8558b3845660f697c28cf615091
khbesbes <5786@holbertonstudents.com>
2022-10-06 08:01:43 -0700

    lower and numbers

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,24 +5,24 @@
  */
 
 int main(void)
-	
+
 {
- int n;
- char c;		    
- 	
+	int n;
+	char c;
+
 	for (n = 48 ; n <= 57 ; n++)
-		{
+	{
 		putchar(n);
-		}
-	for (c = 'a'; c <='f'; c++ )
-		{
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
 		putchar(c);
-		}
-        putchar('\n');
+	}
+	putchar('\n');
 
-	return(0);
+	return (0);
 
-}		
+}
 
 
 
