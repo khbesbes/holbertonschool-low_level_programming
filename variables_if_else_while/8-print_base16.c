8162649bc6c370af95acc61520acf5da4bddfd5c
khbesbes <5786@holbertonstudents.com>
2022-10-06 07:45:48 -0700

    lowercase && numbers

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main -print digit and lowercase
+ * Return: 0
+ */
+
+int main(void)
+	
+{
+ int n;
+ char c;		    
+ 	
+	for (n = 48 ; n <= 57 ; n++)
+		{
+		putchar(n);
+		}
+	for (c = 'a'; c <='f'; c++ )
+		{
+		putchar(c);
+		}
+        putchar('\n');
+
+	return(0);
+
+}		
+
+
+
+
+
+
+
+
+
+
