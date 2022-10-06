b027bf708e85bd2a22aeff8cca28f4f2fe9f46f1
khbesbes <5786@holbertonstudents.com>
2022-10-06 02:48:09 -0700

    counter de num

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print counter of numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		printf("%d", count);
+	}
+	 printf("\n");
+	return (0);
+}
