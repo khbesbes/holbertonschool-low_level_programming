a4681c9c4df3567afd4a327796482cc37ae9502b
khbesbes <5786@holbertonstudents.com>
2022-10-06 06:55:07 -0700

    reverse lower case

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- alphabet inverse
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
