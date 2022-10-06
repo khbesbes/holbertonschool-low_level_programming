3b647c6ce565f7adc533aa6a613c022f1f6ddf3b
khbesbes <5786@holbertonstudents.com>
2022-10-06 01:43:49 -0700

    lowercase != e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print lower case != q & e
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
