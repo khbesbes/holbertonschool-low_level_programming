29d9cb586d01d729bd2c86c7acc293a2a5facd3b
khbesbes <5786@holbertonstudents.com>
2022-10-09 07:02:01 -0700

    update

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef main_h
-#define main_h
+#include " main.h "
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -12,5 +11,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-#endif
+void print_to_98(int 
