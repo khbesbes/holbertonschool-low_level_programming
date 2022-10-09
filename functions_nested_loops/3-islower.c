a17dbdaedd04a2e07dcf33a580a04cd1da25650c
khbesbes <5786@holbertonstudents.com>
2022-10-09 06:33:29 -0700

    islower function

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include " main.h"
+/**
+* main :Write a function that checks for lowercase character. 
+* return : always 0
+*/
+
+
+int _islower(int c);
+ {
+	if(c<='z' && c>= 'a')
+   {		
+       return (1);
+   }    
+	return (0);	
+ }
