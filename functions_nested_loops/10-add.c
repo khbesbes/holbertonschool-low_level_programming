441ed7967ece38e1ac0e1038a33ca8d5bcadaf7a
khbesbes <5786@holbertonstudents.com>
2022-10-10 06:52:32 -0700

    addation operation

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * add: adds two integers and returns the result.
+ * @a:integer ;
+ * @b:integer ; 
+ */
+int add(int a, int b)
+{	
+	int c;
+
+	       	
+       	c = a + b;
+       
+
+       	return(c);
+
+
+}
+
+
+		
+	
