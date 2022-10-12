30845cf07119df49b36571866166cb9e46414602
khbesbes <5786@holbertonstudents.com>
2022-10-12 07:16:26 -0700

    a*b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Write a function that multiplies two integers
+ * @a: input value
+ * @b: input value
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
