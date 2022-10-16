4834db292fe0a51c41311c9f59b8b896805b91f0
khalifa besbes <5786@holbertonstudents.com>
2022-10-16 08:39:09 +0100

    print _

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Write a function that draws a straight line in the terminal.
+ *  Return = (0) ;
+*/
+void print_line(int n)
+{
+int i;
+
+if (n <= 0)
+    {
+        _putchar ('\n');
+    }
+else if (n > 0)
+    {
+        for ( i = 2 ; i <= n ; i++)
+        {
+            _putchar('_');        
+        }
+    }
+    _putchar('\n');    
+}
