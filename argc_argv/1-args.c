f50f3de1b8745da79e0d0a6939d82eb8a2c5b78d
khbesbes <5786@holbertonstudents.com>
2022-11-04 13:29:14 +0100

    hello

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - counter of numbers
+ * @argc: integer
+ * @argv: array of string
+ * Return: always 0;
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
