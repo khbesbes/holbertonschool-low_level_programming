1272d58f5bcbf1a2af3bbf42b633a81aef572997
khbesbes <5786@holbertonstudents.com>
2022-11-14 12:34:21 +0100

    3-islower.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,19 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-int i, x = 1;
-
-if (argc != 3)
+int i, x, s ;
+if ( argc < 3)
 {
-printf("Error\n");
-return (1);
+ printf("0\n");
 }
 else
-
 for (i = 1; i < argc; i++)
 {
-x *= atoi(argv[i]);
+for (j = 0; argv[i][j] != '\0'; j++)
+
+{
+if (argv[i][j] > '9' || argv [i][j] < '0')
+{
+printf ("Error\n");
+return (0);
 }
-printf("%d\n", x);
+x += atoi(argv[i]);
+}
+printf("%d\n",x)
+
+}
 return (0);
 }
--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
-* _islower : Write a function that checks for lowercase character 
+*_islower - Write a function that checks for lowercase character
 * @c: input value
 * Return: always 0
-* 
 */
 int _islower(int c)
 {
-
-
-	if (c<=97 && c>= 122)
+	if (c <= 97 && c >= 122)
+	{
 		return (1);
-   
+	}
+	else
 	return (0);
 }
 
