a2fe8a63af093b381a687e312a64b3720d8eba38
khbesbes <5786@holbertonstudents.com>
2022-11-14 13:41:28 +0100

    up to date

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,15 +1,12 @@
 #include "main.h"
 /**
- *  _isalpha:checks for alphabetic character
- *  Return: (0)
+ * _isalpha - checks for alphabetic character
+ * @c: integer
+ *  Return: Alaways 0
  */
 int _isalpha(int c)
 {
-	if  ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-	{
-		return (1);
-   	}
-	else
-		return (0);	
-	
-}	
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	return (1);
+	return (0);
+}
