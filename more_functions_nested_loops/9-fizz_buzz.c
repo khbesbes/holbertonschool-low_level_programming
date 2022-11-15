5d0caddc67905ac7406390e12f5c3f338dccb4eb
khbesbes <5786@holbertonstudents.com>
2022-11-15 17:30:39 +0100

    add 100 menbers

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - prints the number from 0 to 100
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
