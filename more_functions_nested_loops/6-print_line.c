841df2a5f2d53b9c2d6a00f78d8025045c5d8ff6
khbesbes <5786@holbertonstudents.com>
2022-11-15 10:23:37 +0100

    add up to date

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,15 @@ void print_line(int n)
 int i;
 
 if (n <= 0)
-
+{
 _putchar ('\n');
-
+}
 else if (n > 0)
 {
-for (i = 2 ; i <= n ; i++)
+for (i = 1 ; i <= n ; i++)
+{
 _putchar('_');
-
+}
 }
 _putchar('\n');
 }
