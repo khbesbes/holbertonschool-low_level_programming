ce99ef37563fc8ff000ce0f5d128e4e0ea623805
khbesbes <5786@holbertonstudents.com>
2022-11-15 09:57:13 +0100

    add straight line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * print_line - Write a function that draws a straight line in the terminal.
- *  Return = (0) ;
+ * print_line - Write a function that draws a straight line in terminal.
+ * @n: integer
+ * Return: 0
 */
 void print_line(int n)
 {
@@ -9,13 +10,12 @@ int i;
 
 if (n <= 0)
 
-    _putchar ('\n');
+_putchar ('\n');
 
 else if (n > 0)
-    {    for ( i = 2 ; i <= n ; i++)
-        
-        _putchar('_');        
-    }
-
-    _putchar('\n');
+{
+for (i = 2 ; i <= n ; i++)
+_putchar('_');
+}
+_putchar('\n');
 }
