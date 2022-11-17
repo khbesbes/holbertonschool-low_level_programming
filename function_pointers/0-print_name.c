91940757ca97ad728c4b967a2a28478c355d3817
khbesbes <5786@holbertonstudents.com>
2022-11-17 15:09:07 +0100

    new uptodate

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,8 +2,8 @@
 #include"stdlib.h"
 /**
  * print_name - prints a name.
- * @name: pointer to charcter 
- * @f: pointer to function 
+ * @name: pointer to charcter
+ * @f: pointer to function
  * Return: 0
 */
 void print_name(char *name, void (*f)(char *))
