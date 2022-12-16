b494cf00b276b8b47ec5e895388c03c20889d7cf
khbesbes <5786@holbertonstudents.com>
2022-12-16 14:10:26 +0100

    hello

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,22 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - the progarm that prints the size of various types
- * return : 0
+ * main - Entry point
+ *
+ * Description: 'Just print string'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int intType;
-	long int intType;
-	long long int longType;
-	float floatType;
-	char charType;
+	char myChar;
+	int myInt;
+	long int myLongInt;
+	long long int myLongLongInt;
+	float myFloat;
 
-	printf("Size of int: %d bytes\n", sizeof(intType));
-	printf("size of long int: %d byte\n", sizeof(intType));
-	printf("size of long long int : %dbyte\n", sizeof(longintType));
-	printf("Size of float: %d bytes\n", sizeof(floatType));
-	printf("Size of char: %d byte\n", sizeof(charType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(myChar));
+	printf("Size of an int: %zu byte(s)\n", sizeof(myInt));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(myLongInt));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(myLongLongInt));
+	printf("Size of a float: %zu byte(s)\n", sizeof(myFloat));
 
 	return (0);
 }
