409c5e41039a98e3218807f20fa65d77655ac8c3
khbesbes <5786@holbertonstudents.com>
2022-12-16 14:00:25 +0100

    put function*

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 
 /**
- * main = write a C program that  prints
+ * main - write a C program that  prints
  *
  * Return:0
- */  
-int main(void) 
-{
-        puts("\"Programming is like building a multilingual puzzle");
-  	return (0);
-}
+ */
 
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
