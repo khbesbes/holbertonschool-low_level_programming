8f83b7acd9ca53242c27e7cb7a3b265591400e45
khbesbes <5786@holbertonstudents.com>
2022-12-16 20:12:47 +0100

    add line

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,30 +1,31 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "variadic_functions.h"
+
 /**
- * print_numbers - Entry Point
- * @separator: comma space
- * @n: elements to be printed
+ * print_numbers - Entry point
+ *
+ * @separator: string
+ *
+ * @n: constant unsigned integer
+ *
+ * Description: 'print parameters.'
+ *
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
+	va_list ap;
 
-	va_start(valist, n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, const unsigned int));
-
-		if (i != (n - 1))
+		printf("%d", va_arg(ap, int));
+		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
-	printf("\n");
+	putchar('\n');
 
-	va_end(valist);
+	va_end(ap);
 }
