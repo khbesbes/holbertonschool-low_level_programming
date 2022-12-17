76c12afd0cd4a95383705b1441050f92a72949c8
khbesbes <5786@holbertonstudents.com>
2022-12-17 10:46:17 +0100

    add new

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,34 +1,33 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
- * main  - multipule all operation
- *@argv: array of string
- *@argc: interger
- * Return: 1(Sucess) or 0.
+ * main - Entry point
+ *
+ * @argc : integer
+ * @argv : array of strings
+ *
+ * Description: 'prints the number of arguments passed into it.'
+ *
+ * Return:  1 (Success) or 0
  */
 int main(int argc, char *argv[])
 {
-int i, x, s ;
-if ( argc < 3)
-{
- printf("0\n");
-}
-else
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
+	int x, i, r = 1;
 
-{
-if (argv[i][j] > '9' || argv [i][j] < '0')
-{
-printf ("Error\n");
-return (0);
-}
-x += atoi(argv[i]);
-}
-printf("%d\n",x)
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-}
-return (0);
+	for (i = 1; i < argc; i++)
+	{
+		x = strtol(argv[i], NULL, 10);
+		r = r * x;
+	}
+
+	printf("%d\n", r);
+
+	return (0);
 }
