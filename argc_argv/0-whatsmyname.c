67f25ccae128a59dbb7bbf112835f95709218500
khbesbes <5786@holbertonstudents.com>
2022-12-17 10:51:16 +0100

    what my name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - print name
- * @argc: integer
- * @argv: array to sting
- * Return: Alaways 0
-*/
+ * main - Entry point
+ *
+ * @argc : integer
+ * @argv : array of strings
+ *
+ * Description: 'that prints its name, followed by a new line.'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-argc = 0;
+	argc = 0;
 
-printf("%s\n", argv[argc]);
+	printf("%s\n", argv[argc]);
 
-return (0);
+	return (0);
 }
