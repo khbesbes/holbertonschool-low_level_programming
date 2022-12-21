c6c3fc624c9e5b5f34986ad5484950e80a806ab8
khbesbes <5786@holbertonstudents.com>
2022-12-21 10:29:13 +0100

    hello

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlidtint - prints integers 
+ * @h: pointer to integer
+ * Return: data sturct.
+ */
+ size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *c;
+
+	c = h;
+	while (c != NULL)
+	{
+		printf("%d\n", c->n);
+		c = c->next;
+		i++;
+	}
+	return (i);
+}
+
