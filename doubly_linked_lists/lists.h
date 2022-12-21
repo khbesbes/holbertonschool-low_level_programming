9de977379271aaa98067bb60a8dc28b6d6cbced1
khbesbes <5786@holbertonstudents.com>
2022-12-21 10:37:17 +0100

    hello

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef _LISTS_H
 #define _LISTS_H
-#include <stdio>
-#include <stdlib>
-#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dlistint_s - doubly linked list
