b5d2c6e042d29b2edbf9fc9ccb31a45d424221de
khbesbes <5786@holbertonstudents.com>
2022-12-23 15:35:04 +0100

    strcpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ *
+ * @dest : pointer of string
+ * @src : pointer of string
+ * @n : integer
+ *
+ * Description: 'copy strings.'
+ *
+ * Return: pointer of string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
