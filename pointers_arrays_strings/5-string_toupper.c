8aa33da74bcc265ae3f9ffc6049dce2b2c2e47b4
khbesbes <5786@holbertonstudents.com>
2022-12-23 15:46:03 +0100

    hello

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+include "main.h"
+
+/**
+ * string_toupper - Entry point
+ *
+ * @s : string
+ *
+ * Description: 'to uppercase'
+ *
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = s[i];
+		if (j >= 97 && j <= 122)
+			s[i] = j - 32;
+	}
+
+	return (s);
+}
